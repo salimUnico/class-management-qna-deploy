62d737834e3934cfd23e4105c0516b064d9a4fa5
chandan-002 <chandan.unicoglobal@gmail.com>
2022-08-04 15:12:38 +0530

    dumb or wot

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -4,9 +4,10 @@ const router = express.Router();
 // controller 
 const UserRouter = require('./user');
 const QuestionRouter = require('./qp');
-const NotesRouter = require('./notes');
+const NotesRouter = require('./qp');
 
 router.use('/user', UserRouter);
+router.use('/question', QuestionRouter);
 router.use('/notes', NotesRouter);
 
 module.exports = router;
